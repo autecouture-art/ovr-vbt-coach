9bc01da1a5796f7a5624eac6ddee556811302e2b
Hideyuki Hoshino <autecouture@gmail.com>
2026-04-22 12:56:01 +0900

    Bump iOS build number to 80 for TestFlight

--- app.config.ts
+++ app.config.ts
@@ -31,7 +31,7 @@ const config: ExpoConfig = {
   ios: {
         supportsTablet: true,
     bundleIdentifier: env.iosBundleId,
-    buildNumber: "79",
+    buildNumber: "80",
     infoPlist: {
       ITSAppUsesNonExemptEncryption: false,
       NSBluetoothAlwaysUsageDescription:
